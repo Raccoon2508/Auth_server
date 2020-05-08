2309e49b72f2ff4d5c166e311c5e9938eef26e58
Raccoon2508 <ridge2006@mail.ru>
2020-05-08 14:45:05 +0300

    Update server with reading db.json

--- server/server.ts
+++ server/server.ts
@@ -1,12 +1,23 @@
 const express = require('express');
+const fs = require('fs');
+
 const app = express();
 app.listen(3000, () => {
     console.log ('listen on 3000 port')
 })
 
 
-app.get('/',(req,res) => {
-    res.send('hello, you at homepage');
+let req  = app.use('/login', (req, res, next) => {
+    if(req.query.user && req.query.pass) {
+        console.log(req.query.user);
+        console.log(req.query.pass);
+        next();}
+    else {
+        console.log('Some error on login');
+    }    
 
-    
-})
+}) 
+
+
+fs.promises.readFile('db.json', 'utf-8')
+    .then(data => console.log('Data', data));
